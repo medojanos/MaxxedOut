08467131f9400b0a5a43760debb7ded5b440a8a5
medojanos <medojanos27@gmail.com>
2025-09-11 10:12:43 +0200

    started website

---
+++ Website/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.svg">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>MaxxedOut</title>
+</head>
+<body>
+    <?php
+        include_once "header.htm";
+    ?>
+
+    <section id="hero" class="container-fluid align-items-center d-flex justify-content-center">
+        <a href="#download"><button>Megtekintem</button></a>
+    </section>
+    <section id="download">
+
+    </section>
+    <?php
+        include_once "footer.htm";
+    ?>
+</body>
+</html>
