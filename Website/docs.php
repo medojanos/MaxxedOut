893f711c50b5ad37364c075997a2c13b1600f5f7
medojanos <medojanos27@gmail.com>
2025-09-18 18:28:10 +0200

    initialized mobile app

---
+++ Website/docs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.svg">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>MaxxedOut</title>
+</head>
+<body>
+    <?php
+        include_once "nav.htm";
+    ?>
+    
+    <?php
+        include_once "footer.htm";
+    ?>
+</body>
+</html>
--- Website/readme.js
+++ Website/readme.js
@@ -4,10 +4,6 @@ async function fetchReadme() {
   const url = "https://raw.githubusercontent.com/medojanos/MaxxedOut/refs/heads/main/README.md";
   try {
     const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
     document.getElementById("more").innerHTML = await response.text();
   } catch (error) {
     console.error(error.message);
