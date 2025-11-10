cbab6f8fc6b69441e68b47773b917ef0ca519435
Tekeres Dénes <tekeresdenes@gszi.edu.hu>
2025-11-10 11:20:22 +0100

    Made password strength

--- web/src/Registration.jsx
+++ web/src/Registration.jsx
@@ -6,14 +6,20 @@ export default function Registration() {
   const[password, setPassword] = useState();
   const[repassword, setRepassword] = useState();
   const[status, setStatus] = useState();
+  const[pwdStrength, setPwdStrength] = useState();
   
   function HandleForm(event){
     event.preventDefault();
 
     if(password != repassword) {
       setStatus("Passwords do not match")
-      return;
-    };
+      return
+    }
+
+    if(pwdStrength == "Weak"){
+      setStatus("Password is weak")
+      return
+    }
 
     fetch("http://localhost:4000/register", {
       method: "POST",
@@ -36,11 +42,45 @@ export default function Registration() {
         setEmail(e.target.value);
         break;
       case "password":
-        setPassword(e.target.value);
+        setPassword(e.target.value); 
+        evaluatePwdStrength(e.target.value);
         break;
       case "repassword":
         setRepassword(e.target.value);
         break;
+    }
+  }
+
+  function evaluatePwdStrength(password){
+    let score = 0;
+
+    if(password){
+      if(password.length > 8) score += 2;
+      if(/[a-z]/.test(password)) score++;
+      if(/[A-Z]/.test(password)) score++;
+      if(/[0-9]/.test(password)) score++;
+      if(/[^A-Za-z0-9]/.test(password)) score++;
+  
+      switch (score) {
+        case 0:
+        case 1:
+        case 2:
+          setPwdStrength("Weak");
+          break;
+        case 3:
+        case 4:
+          setPwdStrength("Medium");
+          break;
+        case 5:
+          setPwdStrength("Strong");
+          break;
+        case 6:
+          setPwdStrength("Very strong");
+          break;
+      }
+    }
+    else{
+      setPwdStrength("");
     }
   }
 
@@ -51,9 +91,10 @@ export default function Registration() {
         <input name="email" type="email" autoComplete="on" onInput={HandleInput} required></input><br></br>
         <input name="password" type="password" onInput={HandleInput} required></input><br></br>
         <input name="repassword" type="password" onInput={HandleInput} required></input><br></br>
-        <input type="submit" className="me-2" onInput={HandleInput}></input>
-        <input type="reset"></input><br></br>
-        {status ? status : ""}
+        <p>{pwdStrength ? "Password strength: " + pwdStrength : ""}</p>
+        <input type="submit" className="me-2"></input>
+        <input type="reset" onClick={() => {setPwdStrength(""); setStatus("");}}></input><br></br>
+        {status}
       </form>
     </section>
   )
