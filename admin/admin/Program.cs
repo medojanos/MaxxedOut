ce062e7ba8f8741e83a41c82a04f3ed1f26c6f8c
Medovarszki János <medovarszkijanos@gszi.edu.hu>
2025-11-10 11:18:47 +0100

    Simple look of application

--- admin/admin/Program.cs
+++ admin/admin/Program.cs
@@ -16,7 +16,7 @@ namespace admin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminPanel());
+            Application.Run(new Exercises());
         }
     }
 }
--- admin/admin/Users.cs
+++ admin/admin/Users.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace admin
 {
-    public partial class AdminPanel : Form
+    public partial class Users : Form
     {
-        public AdminPanel()
+        public Users()
         {
             InitializeComponent();
+        }
+
+        private void exercisesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exercises exercises = new Exercises();
+            exercises.Show();
+            this.Hide();
         }
     }
 }
