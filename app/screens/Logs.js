3b9a9715ed1e0496885aa8298e450bb9c056f6d8
medojanos <medojanos27@gmail.com>
2025-11-11 21:09:09 +0100

    Added calendar package

--- app/screens/Logs.js
+++ app/screens/Logs.js
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
-
-import Style from "../style/Style";
+import { View } from "react-native";
+import Style from "../style/Style"
+import { Calendar } from "react-native-calendars"
 
 export default function Logs() {
     return (
-        <View style={Style.container}><Text>Logs</Text></View>
+        <View style={Style.container}>
+            <Calendar></Calendar>
+        </View>
     );
 }
