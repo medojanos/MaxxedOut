d75e62072045d45b0328e45eee4d22a41aeecda6
Medovarszki János <medojanos27@gmail.com>
2026-02-09 20:30:11 +0100

    Added the build to the website

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ export default function Navbar() {
             <div className="container-lg">
                 <Link to="/"><h3>MaxxedOut</h3></Link>
                 <div>
+                    <Link to="https://github.com/medojanos/MaxxedOut" className="me-3">GitHub</Link>
                     <Link to="/docs">Docs</Link>
                 </div>
             </div>
