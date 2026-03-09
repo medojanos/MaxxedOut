067516daee9fa086575896e4e20883e4d5c888ad
medojanos <medojanos27@gmail.com>
2026-03-09 21:04:24 +0100

    Bump version to 0.1.1 in app.config.js

--- app/app.config.js
+++ app/app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "MaxxedOut",
     "slug": "maxxedout",
-    "version": "0.1.0",
+    "version": "0.1.1",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "dark",
