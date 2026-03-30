c4324c191c5fcda32b199739f9b1cb54299e1c7d
Medovarszki János <122732468+medojanos@users.noreply.github.com>
2026-03-31 00:18:16 +0200

    Update documentation links to use 'documents' path

--- web/src/pages/Docs.jsx
+++ web/src/pages/Docs.jsx
@@ -9,9 +9,9 @@ export default function Docs() {
             <h2>Documentation</h2>
             <p>Download whole documentation</p>
             <p>
-                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/main/documentation/Documentation_MaxxedOut-EN.docx" target="_blank">English</a>
+                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/main/documents/Documentation_MaxxedOut-EN.docx" download target="_blank">English</a>
                 <span> | </span>
-                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/main/documentation/Documentation_MaxxedOut-HU.docx" target="_blank">Hungarian</a>
+                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/main/documents/Documentation_MaxxedOut-HU.docx" download target="_blank">Hungarian</a>
             </p>
             <div id="readme" className="content"></div>
         </section>
