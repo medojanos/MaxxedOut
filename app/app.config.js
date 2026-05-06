b7e0054baa1aac7b5759c54feb07cce134feae0b
medojanos <medojanos27@gmail.com>
2026-05-06 15:07:30 +0200

    fix: update app version to 0.1.8 in configuration

--- app/app.config.js
+++ app/app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "MaxxedOut",
     "slug": "maxxedout",
-    "version": "0.1.7",
+    "version": "0.1.8",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "dark",
