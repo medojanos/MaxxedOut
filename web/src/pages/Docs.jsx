4e2ef1ede978d0fc430c3ddd653752658fe13992
medojanos <medojanos27@gmail.com>
2026-05-19 08:47:59 +0200

    fix: update documentation links to PDF format for English and Hungarian versions

--- web/src/pages/Docs.jsx
+++ web/src/pages/Docs.jsx
@@ -9,9 +9,9 @@ export default function Docs() {
             <h2>Documentation</h2>
             <p>Download whole documentation</p>
             <p>
-                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/refs/heads/main/documents/Documentation_MaxxedOut-EN.docx">English</a>
+                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/refs/heads/main/documents/Documentation_MaxxedOut-EN.pdf">English</a>
                 <span> | </span>
-                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/refs/heads/main/documents/Documentation_MaxxedOut-HU.docx">Hungarian</a>
+                <a href="https://raw.githubusercontent.com/medojanos/MaxxedOut/refs/heads/main/documents/Documentation_MaxxedOut-HU.pdf">Hungarian</a>
             </p>
             <div id="readme" className="content"></div>
         </section>
